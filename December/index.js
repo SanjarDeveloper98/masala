e16a9e97a306b228d7b88acbe72867604996e694
SanjarDelevoper98 <s981114s@gmail.com>
2023-01-04 18:27:57 +0500

    sanjar developer

---
+++ December/index.js
@@ -0,0 +1,44 @@
+
+// function fun() {
+//     var args = [];
+
+//     for (var i = 1; i < arguments.length; i++) {
+//       args.push(arguments[i]);
+//     }
+
+//     return args;
+//   }
+
+//   fun(1, 2, 3, 4, 5); //=> [2, 3, 4, 5]
+
+
+
+
+// function myFunction(a) {
+//     const isNegative = num => num < 0;
+//     return a.filter(isNegative).length;
+// }
+
+// console.log(myFunction(isNegative));
+
+
+
+
+// function myFunction(a) {
+//     const isNegative = num => num < 0;
+//     return a.filter(isNegative).length;
+// }
+
+// console.log(myFunction(isNegative));
+
+
+// unli harflarni hisoblash
+
+
+// // 
+
+//   function myFunction(a, n) {
+ 
+//     return 
+// }
+
